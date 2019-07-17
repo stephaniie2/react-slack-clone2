d68e0675ebb40df5d849be9e6ab085db2531ab11
Åke Hallberg <akehallberg@Akes-iMac.lan>
2019-07-17 20:24:26 +0200

    SidePanel and ClearUser / Signout Functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,29 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from "react";
+import { Grid } from "semantic-ui-react";
+import "./App.css";
+import { connect } from "react-redux";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        App
-      </div>
-    );
-  }
-}
+import ColorPanel from "./ColorPanel/ColorPanel";
+import SidePanel from "./SidePanel/SidePanel";
+import MetaPanel from "./MetaPanel/MetaPanel";
+import Messages from "./Messages/Messages";
 
-export default App;
+const App = ({ currentUser }) => (
+  <Grid columns="equal" className="app" style={{ background: "#eee" }}>
+    <ColorPanel />
+    <SidePanel currentUser={currentUser} />
+    <Grid.Column style={{ marginLeft: 320 }}>
+      <Messages />
+    </Grid.Column>
+
+    <Grid.Column width={4}>
+      <MetaPanel />
+    </Grid.Column>
+  </Grid>
+);
+
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(App);
