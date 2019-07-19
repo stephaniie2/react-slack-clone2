da69ae814bcfe19b26963c12b24082dedabfb378
Åke Hallberg <akehallberg@Akes-iMac.lan>
2019-07-19 12:39:40 +0200

    Adding / Displaying Messages on Public Channels

--- src/components/App.js
+++ src/components/App.js
@@ -8,12 +8,16 @@ import SidePanel from "./SidePanel/SidePanel";
 import MetaPanel from "./MetaPanel/MetaPanel";
 import Messages from "./Messages/Messages";
 
-const App = ({ currentUser }) => (
+const App = ({ currentUser, currentChannel }) => (
   <Grid columns="equal" className="app" style={{ background: "#eee" }}>
     <ColorPanel />
-    <SidePanel currentUser={currentUser} />
+    <SidePanel key={currentUser && currentUser.uid} currentUser={currentUser} />
     <Grid.Column style={{ marginLeft: 320 }}>
-      <Messages />
+      <Messages
+        key={currentChannel && currentChannel.id}
+        currentChannel={currentChannel}
+        currentUser={currentUser}
+      />
     </Grid.Column>
 
     <Grid.Column width={4}>
@@ -23,7 +27,8 @@ const App = ({ currentUser }) => (
 );
 
 const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+  currentUser: state.user.currentUser,
+  currentChannel: state.channel.currentChannel
 });
 
 export default connect(mapStateToProps)(App);
